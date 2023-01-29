7d6e4bbbc712b48a3216bf991cd39b0b3d433d02
vishnu9651 <vishnukantee0044@gmail.com>
2023-01-30 01:15:47 +0530

    data

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const jsonServer=require("json-server");
-const server=jsonServer.create();
+const express = require("express")
 
-const router=jsonServer.router(db.json);
-const middlewares=jsonServer.defaults()
-const port=process.env.PORT || 3001;
-server.use(middlewares)
-server.use(router)
-server.listen(port)
+const elecData=require("./electronics.json")
+const groceryData=require("./grocery.json")
+const fashionData=require("./fashion.json")
+const cors=require("cors")
+
+
+const port =8080
+const app = express()
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json())
+
+
+app.get("/electronics", (req, res) => {
+    res.send(elecData.electronics)
+})
+app.get("/fashion", (req, res) => {
+    res.send(fashionData.fashion)
+})
+app.get("/grocery", (req, res) => {
+    res.send(groceryData.grocery)
+})
+
+app.listen(port, async () => {
+    try {
+    
+        console.log("connected to db")
+    } catch (err) {
+        console.log(err)
+    }
+    console.log("port is running at 8080")
+})
